77949c6e2eee99752f431d1703aede3a0a708f50
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-08 22:39:34 +0200

    Modified if control (Need to be stricly = true)

--- components/header.php
+++ components/header.php
@@ -2,11 +2,14 @@
 <span>Cryinair</span>
 
 <?php
-	require_once("../utility/db.php"); 
-	require_once("../utility/utility.php");
-	if (session_status() == PHP_SESSION_NONE) 
-    	sec_session_start();
+require_once "../utility/db.php";
+require_once "../utility/utility.php";
+if (session_status() == PHP_SESSION_NONE) {
+	sec_session_start();
+}
 
-	if(login_check($conn) == true)
-		echo "<br/><p>Welcome back: " . $_SESSION["username"] . "</p>";
+if (login_check($conn) === true) {
+	echo "<br/><p>Welcome back: " . $_SESSION["username"] . "</p>";
+}
+
 ?>
