f72d4ad0001eae52d563bae2936f0809688be0ba
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-08 22:40:15 +0200

    Modified if control (Need to be stricly = true)

--- components/navbar.php
+++ components/navbar.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 
-	require_once("../utility/db.php");
-	require_once("../utility/utility.php");
-	if (session_status() == PHP_SESSION_NONE) 
-		sec_session_start();
+require_once "../utility/db.php";
+require_once "../utility/utility.php";
+if (session_status() == PHP_SESSION_NONE) {
+	sec_session_start();
+}
 
-	if(login_check($conn))
-		echo "<a href='#' id='logoutLink' >Logout</a>
+if (login_check($conn) === true) {
+	echo "<a href='#' id='logoutLink' >Logout</a>
 		<a href='#' id='buyLink'>Buy</a>
 		<a href='#' id='updateLink' >Update</a>";
-	else
-		echo "<a href='#' id='updateLink' >Homepage</a>
+} else {
+	echo "<a href='#' id='updateLink' >Homepage</a>
 			<a href='#' id='registerLink' >Register</a>
 		<a href='#' id='loginLink' >Login</a>";
-		
+}
+
 ?>
