75fd83e1b860ee24a4ed87225c03f78c0a72cb65
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-17 19:19:20 +0200

    Added file with all the defines

---
+++ utility/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('SERVERNAME', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DATABASE', 'airplane_reservation');
+
+define('AIRPLANE_WIDTH', 6);
+define('AIRPLANE_LENGTH', 10);
+
+define('SESSION_EXPIRE_TIME', 120);
