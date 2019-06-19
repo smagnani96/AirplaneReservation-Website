e76c58db0e2e1b9ce16c5c7ab2636a27c47574a4
Simone Magnani <simonemagnani.96@gmail.com>
2019-06-20 00:23:52 +0200

    Fixed https check

--- utility/checkerHttps.php
+++ utility/checkerHttps.php
@@ -2,7 +2,7 @@
 
 require_once "checkerAccess.php";
 
-if ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'off') || $_SERVER['SERVER_PORT'] != 443) {
+if ($_SERVER['HTTPS'] !== 'on') {
 	echo json_encode(array('err' => -1, 'msg' => "To perform that operation you must access through HTTPS."));
 	die();
 }
